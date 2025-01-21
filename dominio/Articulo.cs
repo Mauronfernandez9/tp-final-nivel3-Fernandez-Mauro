58976e6b507ce97750c155d3c582e8ea8a2c5d0f
Mauro Fernandez <mauronfernandez9@gmail.com>
2025-01-21 17:14:12 -0300

    Creacion de dominio

---
+++ dominio/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public int CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca MarcaArticulo { get; set; }
+        public Categoria CategoriaArticulo { get; set; }
+        public string UrlImagen { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
---
+++ dominio/Favorito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Favorito
+    {
+        public int Id { get; set; }
+        public Usuario usuario { get; set; }
+        public List<Articulo> articulosFavoritos { get; set; }
+    }
+}
